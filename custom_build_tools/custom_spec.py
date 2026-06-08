1d0f35ab7d9ca6df79843c36d44844b75f84e52e
Tom Björkholm <klausuler_linnet0q@icloud.com>
2026-06-08 14:34:03 +0200

    WIP

--- custom_build_tools/custom_spec.py
+++ custom_build_tools/custom_spec.py
@@ -3,9 +3,15 @@
 
 from pathlib import Path
 from typing import Optional
+import sys
 from build_spec import BuildSpec
+CUSTOM_BUILD_TOOLS_SRC = Path(__file__).resolve().parent / 'src'
+sys.path.insert(0, str(CUSTOM_BUILD_TOOLS_SRC))
+# pylint: disable=wrong-import-order,wrong-import-position
+from create_pypi_readme import create_pypi_readme_cmd
 
 
 def custom_spec() -> Optional[BuildSpec]:
     """Return custom build spec for this repository."""
-    return BuildSpec(python_layout_max_name_length=25)
+    return BuildSpec(python_layout_max_name_length=25,
+                     custom_after_test=[create_pypi_readme_cmd])
