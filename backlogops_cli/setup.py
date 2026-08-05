953b28052cb2e195ed6a77a86ec2ac675abcc36e
Tom Björkholm <klausuler_linnet0q@icloud.com>
2026-08-05 15:54:16 +0200

    bump version number for new development

--- backlogops_cli/setup.py
+++ backlogops_cli/setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
   name='backlogops-cli',
-  version='0.7',
+  version='0.7.1',
   description='Command line interface for backlog operations.',
   author='Tom Björkholm',
   author_email='klausuler_linnet0q@icloud.com',
@@ -15,7 +15,7 @@ setup(
   package_data={'backlogops_cli': ['src/py.typed']},
   install_requires=[
     'argcomplete >= 3.7.1',
-    'backlogops >= 0.7',
+    'backlogops >= 0.7.1',
     'wizard-ui-bridge[textual] >= 1.2',
     'versionreporter >= 0.4'
   ]
